5dc3595091daaf876368d33d9eb5ed7c1002c6bd
Stefan Billeb <stefan.billeb@gmx.net>
2015-12-30 22:07:00 +0100

    Add Arrays and For Loops Add Dictionary example

--- swift-quickstart.playground/Contents.swift
+++ swift-quickstart.playground/Contents.swift
@@ -74,3 +74,55 @@ class TipCalcuator {    // Subclassing: Foo: SuperBar, SuperFooBar, ...
 
 let tipCalc = TipCalcuator(total: 33.25, taxPct: 0.06)
 tipCalc.printPossibleTips()
+
+//
+// Arrays and For Loops
+let possibleTipsInferred = [0.15, 0.18, 0.20]
+let possibleTipsExplicit:[Double] = [0.15, 0.18, 0.20]
+
+for possibleTip in possibleTipsInferred {
+    print("\(possibleTip*100)%: \(tipCalc.calcTipWithTipPct(possibleTip))")
+}
+
+for i in 0..<possibleTipsExplicit.count { // you can also replace i with _ as an unnamed and unused variable in Swift 2
+    let possibleTip = possibleTipsExplicit[i]
+    print("\(possibleTip*100)%: \(tipCalc.calcTipWithTipPct(possibleTip))")
+
+}
+
+//
+// Dictionaries
+class TipCalculatorWithDictionary {
+    
+    let total: Double
+    let taxPct: Double
+    let subtotal: Double // all properties need initial value here or init method, otherwise: Error!
+    
+    // here comes the initializer - you can have 1+ but different signature
+    init(total: Double, taxPct: Double) {
+        self.total = total
+        self.taxPct = taxPct
+        subtotal = total / (taxPct + 1)
+    }
+    
+    func calcTipWithTipPct(tipPct: Double) -> Double { // -> Double indicates what is returned
+        return subtotal * tipPct
+    }
+    
+    func returnPossibleTips() -> [Int: Double] { // this return value is dictionary
+        // the key is an Int, the value is Double
+        // [Int: Double] is short for Dictionary<Int, Double>
+        let possibleTipsInferred = [0.15, 0.18, 0.20]
+        
+        var retval = [Int: Double]()
+        for possibleTip in possibleTipsInferred {
+            let intPct = Int(possibleTip*100)
+            retval[intPct] = calcTipWithTipPct(possibleTip)
+        }
+        return retval
+        
+    }
+}
+
+var tipCalcDict = TipCalculatorWithDictionary(total: 33.25, taxPct: 0.06)
+tipCalcDict.returnPossibleTips()
